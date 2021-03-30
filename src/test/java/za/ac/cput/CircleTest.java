e657d40ad8efcf1243ebe72ee6a92a0be3cc61f7
Thokozile Snono <thokozilesinono>
2021-03-30 19:29:28 +0200

    First commit

---
+++ src/test/java/za/ac/cput/CircleTest.java
@@ -0,0 +1,22 @@
+package za.ac.cput;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CircleTest {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(5);
+        Circle circle2 = new Circle(15);
+        Circle circle3 = new Circle(20);
+
+        @BeforeEach
+        public void setUp() {
+            circle1 = new Circle();
+            circle2 = new Circle();
+            circle3 = new circle1;
+        }
+
+    }
+}
