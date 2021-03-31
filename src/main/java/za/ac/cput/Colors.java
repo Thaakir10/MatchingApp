50aed44c219d5da9fba50f88f51d3244aa0bb9b8
Thaakir <thaakir10@gmail.com>
2021-03-31 13:24:01 +0200

    Thaakir Ajouhaar - 217244394
    TDD

---
+++ src/main/java/za/ac/cput/Colors.java
@@ -0,0 +1,27 @@
+package za.ac.cput;
+
+/**
+ * Thaakir Ajouhaar - 217244394
+ * Assignment 1 2021
+ *
+ */
+
+public class Colors {
+
+    private String colors;
+
+    public String getColors() {
+        return colors;
+    }
+
+    public void setColors(String colors) {
+        this.colors = colors;
+    }
+
+    @Override
+    public String toString() {
+        return "Colors{" +
+                "colors='" + colors + '\'' +
+                '}';
+    }
+}
